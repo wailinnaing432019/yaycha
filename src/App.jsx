8011f02a679731a943cacba4de9561d9012c6574
wailinnaing432019 <wailinnaing432019@gmail.com>
2024-12-18 13:38:47 +0630

    second test

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ export default function App(){
     <div  style={{ maxWidth: 600, margin: "20px auto" }}>
       <h1>Hello Yaycha!</h1>
       <List>
-        <Item content="Hello , World!" name="Alice" />
+        <Item content="Hello , World!" name="Wai Gyi" />
         <Item content="React is fun" name="Bob" />
       </List>
     </div>
