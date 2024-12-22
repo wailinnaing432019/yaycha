ee194261d458bcbff540e8bcc487a37b8e525348
wailinnaing432019 <wailinnaing432019@gmail.com>
2024-12-22 09:28:25 +0630

    usage useState and looping map

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
+import { useState } from "react";
 import Item from "./Item";
 import List from "./List";
 
-
-
 export default function App(){
+
+
+  const [data,setDate]=useState([
+    {id:1,name:"Alice Cho",content:"Beauty Blogger"},
+    {id:2,name:"Do Pauk",content:"Entertainment"},
+    {id:3,name:"Kelvin",content:"Funny Entertainment"}
+  ]
+  );
   return (
     <div  style={{ maxWidth: 600, margin: "20px auto" }}>
       <h1>Hello Yaycha!</h1>
       <List>
-        <Item content="Hello , World!" name="Wai Gyi" />
-        <Item content="React is fun" name="Bob" />
+        {data.map(item=>{
+          return <Item name={item.name} content={item.content} id={item.id} />
+        })}
       </List>
     </div>
   )
