87ea45003b0e535d6c70b4a2337a449b755d4e70
wailinnaing432019 <wailinnaing432019@gmail.com>
2024-12-24 15:18:26 +0630

    adding design to yaycha

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,22 @@ import List from "./List";
 export default function App(){
 
 
-  const [data,setDate]=useState([
+  const [data,setData]=useState([
     {id:1,name:"Alice Cho",content:"Beauty Blogger"},
     {id:2,name:"Do Pauk",content:"Entertainment"},
     {id:3,name:"Kelvin",content:"Funny Entertainment"}
   ]
   );
+
+  const remove=id=>{
+    setData(data.filter(item=>item.id!==id))
+  };
   return (
     <div  style={{ maxWidth: 600, margin: "20px auto" }}>
       <h1>Hello Yaycha!</h1>
       <List>
         {data.map(item=>{
-          return <Item name={item.name} content={item.content} id={item.id} />
+          return <Item item={item} key={item.id} remove={remove} />
         })}
       </List>
     </div>
--- src/Item.jsx
+++ src/Item.jsx
@@ -1,8 +1,13 @@
-function Item({content,name}){
+function Item({item,remove}){
+
     return (
+      <>
       <li
-      style={{ padding: 10, borderBottom: "1px solid #ddd" }}
-      >{content}-<b>{name}</b></li>
+      style={{ padding: 10, borderBottom: "1px solid #ddd", display:"flex", justifyContent:"space-between" }}
+      ><span>{item.content}-<b>{item.name}</b></span>
+      <button onClick={()=>remove(item.id)}>Remove</button>
+      </li>
+      </>
     )
 }
 export default Item
